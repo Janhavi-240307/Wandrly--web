00152de5b95c11e02f7fe534eaab102606d92583
Janhavi <janhavi746578@gmail.com>
2025-12-30 19:40:28 +0530

    Fix review delete button visibility

--- app.js
+++ app.js
@@ -96,9 +96,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews/", reviewRouter);    // /listings/:id/reviews/: Parent Route
 app.use("/", userRouter);
  
-// app.get("/", (req, res) => {
-//     res.send("Hi!, I am root");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.all(/.*/, (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
--- init/index.js
+++ init/index.js
@@ -1,26 +1,26 @@
+require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/Wandrly";
-
-main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+const dbUrl = process.env.ATLASDB_URL;
+console.log("DB URL:", dbUrl);
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
+  console.log("Connected to MongoDB Atlas");
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: "691ca68fb1a014094159c496" }));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
-}
+  await Listing.deleteMany({});
+  initData.data = initData.data.map(obj => ({
+    ...obj,
+    owner: "691ca68fb1a014094159c496"
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data initialized");
+};
 
-initDB();
+main()
+  .then(initDB)
+  .catch(err => console.log(err));
